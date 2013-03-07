da68d29eb683b078d519bfba1d260f1afbbe9256
bender20112 <netant@inbox.ru>
2013-03-07 13:12:29 +0200

    Created empty project.

---
+++ logback-example/src/main/java/pdp_example/logback_example/App.java
@@ -0,0 +1,13 @@
+package pdp_example.logback_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
